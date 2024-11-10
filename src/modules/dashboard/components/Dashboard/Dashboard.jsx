92302ad0bcbf392f42e2c09686751bec7978feea
siegfried2020 <domydodo2015@gmail.com>
2024-11-10 20:01:35 +0200

    Small Update

--- src/modules/dashboard/components/Dashboard/Dashboard.jsx
+++ src/modules/dashboard/components/Dashboard/Dashboard.jsx
@@ -7,11 +7,15 @@ export default function Dashboard({loginData}) {
     {'This is a welcoming screen for the entry of the application , you can now see the options'}/>
     <div className="fill-recipe-container my-3 mx-3 p-5 d-flex justify-content-between align-items-center">
       <div>
-        Dashboard
+        <h2>Fill the <span className="text-success fw-bold">Recipes</span></h2>
+        <p>you can now fill the meals easily using the table and form, 
+          click here and sill it with the table
+        </p>
+
       </div>
 
       <div>
-        <button>Fill Recipes</button>
+        <button className="btn btn-success px-5">Fill Recipes</button>
       </div>
     </div>
   </div>);
